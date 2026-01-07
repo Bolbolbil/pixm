82fa8f199b328c1b91ebbc8dec8d8a42e22d62da
Bolbolbil <belgacembb1981@gmail.com>
2026-01-07 16:28:15 +0100

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,118 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        blue: {
+          primary: "#0D6EFD",
+        },
+        orange: {
+          background: "#FD384F",
+          hover: "#e23246",
+          primary: "#FA6338",
+          seconadry: "#d3031c",
+          border: "#ffe6e7",
+        },
+        main: {
+          primary: "#2B2B2B",
+          secondary: "#757575",
+        },
+        purple: {
+          primary: "#7E3FF2",
+          secondary: "#A480F2",
+          light: "#E0DDE3",
+        },
+        gray: {
+          dark: "#2B2B2B",
+          light: "#F2F2F2",
+          medium: "#E0DDE3",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "scroll-text": {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "scroll-text": "scroll-text 4s linear infinite",
+      },
+      backgroundImage: {
+        home: "url(/public/assets/images/home-wallpaper.webp)",
+      },
+      transitionTimingFunction: {
+        "bezier-1": "cubic-bezier(.645,.045,.355,1)",
+      },
+      maxWidth: {
+        container: "1200px",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
